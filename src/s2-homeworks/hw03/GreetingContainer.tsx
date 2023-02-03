5fadf79a8259a00e844e5a64ad0b4bf98e099e30
IvanBichan <ivanbichanfe@gmail.com>
2023-02-03 22:36:29 +0300

    - fix: fix error HW3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -42,11 +42,16 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        if(e.currentTarget.value.trim() === '') {
+            setError('Ошибка! введите имя!')
+        }else {
+            let newName = e.currentTarget.value
+            setName(newName) // need to fix
+            error && setError('')
+        }
 
-        let newName = e.currentTarget.value
-        setName(newName) // need to fix
 
-        error && setError('')
+        //error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
