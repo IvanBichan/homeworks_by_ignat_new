1bb5d6d1ca5c502349755aea43fb410bdfb2e7f7
IvanBichan <ivanbichanfe@gmail.com>
2023-03-04 19:03:03 +0300

    - fix: fix view of month in HW9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,7 +33,7 @@ function Clock() {
     }
 
     const stringTime = `${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}:${("0" + date.getSeconds()).slice(-2)}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${("0" + date.getDate()).slice(-2)}.${date.getMonth() + 1}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${("0" + date.getDate()).slice(-2)}.${ ((date.getMonth()) + 1) >= 10? (date.getMonth()) + 1 : "0"+(date.getMonth() + 1)}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] || <br/> // пишут студенты
