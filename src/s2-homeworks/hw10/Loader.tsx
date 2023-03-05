a5698cef7f69a33e60e5c6735fcd9827ab927ebb
IvanBichan <ivanbichanfe@gmail.com>
2023-03-05 14:58:03 +0300

    - feat: add HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
+import p4 from './s3.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <img src={p4} alt={'Loading'} className={s.ellipse}/>
+</div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,9 +1,15 @@
 const initState = {
     isLoading: false,
 }
+export type InitialType = {
+    isLoading: boolean
+}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: InitialType = initState, action: LoadingActionType): InitialType => { // fix any
     switch (action.type) {
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
         // пишет студент  // need to fix
 
         default:
