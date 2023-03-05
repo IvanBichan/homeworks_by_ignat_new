798805e821b2b6432ae32449ab5b942772d71303
IvanBichan <ivanbichanfe@gmail.com>
2023-03-05 17:27:43 +0300

    - feat: add HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,25 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type ThemeReducerType = {
+    themeId:number
+}
+
+
+export const themeReducer = (state:ThemeReducerType = initState, action: ChangeThemeIdType): ThemeReducerType => { // fix any
     switch (action.type) {
+        case "SET_THEME_ID": {
+            return {...state, themeId:action.id}
+        }
         // дописать
+
 
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdType = {
+    type:'SET_THEME_ID',
+    id:number
+}
+export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
